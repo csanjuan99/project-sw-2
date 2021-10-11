e8e723f28f43c5f025db1bce5e413307b5143a64
Carlos Daniel Sanjuan <84832339+csanjuan99@users.noreply.github.com>
2021-10-10 23:43:37 -0500

    Modulo administrador

--- src/main/java/com/app/medyapp/enums/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.app.medyapp.enums;
-
-public enum ERole {
-    //El aplicativo tiene dos roles:
-    // ADMIN(Tiene todos los permisos CRUD) y USER(Gestiona su informacion propia)
-    ROLE_ADMIN, ROLE_USER
-}
--- src/main/java/com/app/medyapp/repository/SubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.medyapp.repository;
-
-import com.app.medyapp.model.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
-}
